d1277499108fce2df3e6dc666206f4428ad8e8ba
zhnglab <61349499+zhnglab@users.noreply.github.com>
2025-11-06 09:54:02 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -59,7 +59,7 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
-PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 
 # 第三方软件包 合并
 # ======== shell/custom-packages.sh =======
