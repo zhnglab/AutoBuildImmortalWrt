aa2f6851acccf628196719e46ffe596fb71bbfe0
zhnglab <61349499+zhnglab@users.noreply.github.com>
2026-06-20 15:46:21 +0800

    Remove obsolete MzWrt x86 subproject

--- mzwrt-x86/packages24.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CUSTOM_PACKAGES=""
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-store"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp luci-i18n-partexp-zh-cn"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc"
